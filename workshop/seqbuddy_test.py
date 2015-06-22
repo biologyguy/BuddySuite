797b4bdbce49f90b52a576bf45a8db30e7b897b9
biologyguy <biologyguy@gmail.com>
2015-06-22 14:22:54 -0400

    Add peptide files to order_feature_alpha test

--- workshop/seqbuddy_test.py
+++ workshop/seqbuddy_test.py
@@ -17,9 +17,9 @@ def seqs_to_hash(_seqbuddy):
     elif _seqbuddy.out_format == "phylipis":
         write_file.write(Sb.phylipi(_seqbuddy, "strict"))
     else:
-        SeqIO.write(_seqbuddy.records, write_file.path, _seqbuddy.out_format)
+        _seqbuddy.write(write_file.path)
 
-    seqs_string = write_file.read().encode()
+    seqs_string = "{0}\n".format(write_file.read().strip()).encode()
     _hash = md5(seqs_string).hexdigest()
     return _hash
 
@@ -28,10 +28,10 @@ root_dir = os.getcwd()
 def resource(file_name):
     return "{0}/unit_test_resources/{1}".format(root_dir, file_name)
 
-seq_files = ["Mnemiopsis_cds.fa", "Mnemiopsis_cds.gb", "Mnemiopsis_cds.nex", "Mnemiopsis_cds.phyr",
-             "Mnemiopsis_cds.stklm",
-             "Mnemiopsis_pep.fa", "Mnemiopsis_pep.gb", "Mnemiopsis_pep.nex", "Mnemiopsis_pep.phyr",
-             "Mnemiopsis_pep.stklm"]
+seq_files = ["Mnemiopsis_cds.fa", "Mnemiopsis_cds.gb", "Mnemiopsis_cds.nex", "Mnemiopsis_cds.phy",
+             "Mnemiopsis_cds.phyr", "Mnemiopsis_cds.stklm",
+             "Mnemiopsis_pep.fa", "Mnemiopsis_pep.gb", "Mnemiopsis_pep.nex", "Mnemiopsis_pep.phy",
+             "Mnemiopsis_pep.phyr", "Mnemiopsis_pep.stklm"]
 
 @pytest.mark.parametrize("seq_file", seq_files)
 def test_instantiate_seqbuddy_from_file(seq_file):
@@ -55,33 +55,16 @@ formats = ["fasta", "gb", "nexus", "phylip-relaxed", "stockholm",
            "fasta", "gb", "nexus", "phylip-relaxed", "stockholm"]
 
 # fa gb nex phy phyr stklm
-hashes = ["25073539df4a982b7f99c72dd280bb8f", "ffa7cb60cb98e50bc4741eed7c88e553", "cb1169c2dd357771a97a02ae2160935d",
-          "99d522e8f52e753b4202b1c162197459", "228e36a30e8433e4ee2cd78c3290fa6b"]
+
+#'-ofa', '--order_features_alphabetically'
+hashes = ["25073539df4a982b7f99c72dd280bb8f", "0ca1c910e2d566556e839c3ca29e162d", "cb1169c2dd357771a97a02ae2160935d",
+          "d1524a20ef968d53a41957d696bfe7ad", "99d522e8f52e753b4202b1c162197459", "228e36a30e8433e4ee2cd78c3290fa6b",
+          "c10d136c93f41db280933d5b3468f187", "2cd6ab6a922a6695ba2ffdbfa81f740d", "8b6737fe33058121fd99d2deee2f9a76",
+          "40f10dc94d85b32155af7446e6402dea", "b229db9c07ff3e4bc049cea73d3ebe2c", "f35cbc6e929c51481e4ec31e95671638"]
 
 hashes = [(sb_objects[indx], value) for indx, value in enumerate(hashes)]
 @pytest.mark.parametrize("seqbuddy,next_hash", hashes)
 def test_order_features_alphabetically(seqbuddy, next_hash):
     tester = Sb.order_features_alphabetically(seqbuddy)
-    if seqbuddy.out_format == "phylip-relaxed":
-        tester.write("troubleshooting.txt")
     assert seqs_to_hash(tester) == next_hash
 
-
-"""
-@pytest.mark.parametrize("seqbuddy,hash", seq_files)
-def test_order_features_alphabetically_reverse(fasta_cds, fasta_pep, gb_cds, gb_pep):
-    tester = Sb.order_features_alphabetically(gb_cds)
-    assert seqs_to_hash(tester, "gb") == "0ca1c910e2d566556e839c3ca29e162d"
-
-    tester = sb.order_features_alphabetically(gb_cds, True)
-    assert seqs_to_hash(tester, "gb") == "71bc8a8d19e5c7a6c54db4196f632cea"
-
-    tester = sb.order_features_alphabetically(gb_pep)
-    assert seqs_to_hash(tester, "gb") == "2cd6ab6a922a6695ba2ffdbfa81f740d"
-
-    tester = sb.order_features_alphabetically(gb_pep, True)
-    assert seqs_to_hash(tester, "gb") == "172f886830e8f8880c31417ec1821490"
-
-    tester = sb.order_features_alphabetically(fasta_cds)
-    assert seqs_to_hash(tester, "fasta") == "25073539df4a982b7f99c72dd280bb8f"
-"""
