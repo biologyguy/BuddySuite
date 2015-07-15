4a581a88b51ca1e54a0c4133151fe8628bd1c19d
biologyguy <biologyguy@gmail.com>
2015-07-15 15:45:26 -0400

    Implement translate() function
    Beefy function to deal with all the possible weirdness from Biopython... Unit tests covering the whole thing.

--- workshop/alignbuddy_test.py
+++ workshop/alignbuddy_test.py
@@ -9,20 +9,13 @@ from io import StringIO
 import re
 from copy import deepcopy
 from Bio.Alphabet import IUPAC
+from Bio.Seq import Seq
 import SeqBuddy as SB
 
 try:
     import workshop.AlignBuddy as Alb
 except ImportError:
     import AlignBuddy as Alb
-
-
-def make_copies(_alignbuddy):
-    copies = deepcopy(_alignbuddy)
-    copies.alpha = _alignbuddy.alpha
-    for _rec in Alb._get_seq_recs(copies):
-        _rec.alphabet = _alignbuddy.alpha
-    return copies
 
 
 def align_to_hash(_alignbuddy, mode='hash'):
@@ -60,6 +53,8 @@ file_types = ["nexus", "phylip", "phylip-relaxed", "stockholm",
               "nexus", "phylip", "phylip-relaxed", "stockholm",
               "phylip", "phylip-relaxed", "stockholm",
               "phylip-relaxed", "stockholm"]
+
+nucl_indices = [0, 1, 2, 3, 11, 12]
 
 input_tuples = [(next_file, file_types[indx]) for indx, next_file in enumerate(align_files)]
 
@@ -191,7 +186,7 @@ def test_write1(alignbuddy, next_hash):
 
 
 def test_write2():  # Unloopable components
-    tester = make_copies(alb_objects[8])
+    tester = Alb._make_copies(alb_objects[8])
     tester.out_format = "fasta"
     with pytest.raises(ValueError):
         str(tester)
@@ -199,7 +194,7 @@ def test_write2():  # Unloopable components
     tester.alignments = []
     assert str(tester) == "AlignBuddy object contains no alignments.\n"
 
-    tester = make_copies(alb_objects[2])
+    tester = Alb._make_copies(alb_objects[2])
     tester.out_format = "phylipi"
     assert md5(str(tester).encode()).hexdigest() == "0379295eb39370bdba17c848ec9a8b73"
 
@@ -273,7 +268,7 @@ lc_hashes = ["cb1169c2dd357771a97a02ae2160935d", "f59e28493949f78637691caeb617ab
              "f3e98897f1bbb3d3d6c5507afa9f814e", "c0dce60745515b31a27de1f919083fe9", "e4d6766b7544557b9ddbdcbf0cde0c16",
              "12716bad78b2f7a40882df3ce183735b", "00661f7afb419c6bb8c9ac654af7c976"]
 
-hashes = [(make_copies(alb_objects[indx]), uc_hash, lc_hashes[indx]) for indx, uc_hash in enumerate(uc_hashes)]
+hashes = [(Alb._make_copies(alb_objects[indx]), uc_hash, lc_hashes[indx]) for indx, uc_hash in enumerate(uc_hashes)]
 
 
 @pytest.mark.parametrize("alignbuddy,uc_hash,lc_hash", hashes)
@@ -286,12 +281,12 @@ def test_cases(alignbuddy, uc_hash, lc_hash):
 # ###########################################  'ca', '--condon_alignment' ############################################ #
 hashes = ["c907d29434fe2b45db60f1a9b70f110d", "f150b94234e93f354839d7c2ca8dae24", "6a7a5416f2ce1b3161c8b5b8b4b9e901",
           "54d412fbca5baa60e4c31305d35dd79a", "3ddc2109b15655ef0eed3908713510de", "f728ab606602ed67357f78194e500664"]
-indices = [0, 1, 2, 3, 11, 12]
-hashes = [(make_copies(alb_objects[indices[indx]]), next_hash) for indx, next_hash in enumerate(hashes)]
+
+hashes = [(Alb._make_copies(alb_objects[nucl_indices[indx]]), next_hash) for indx, next_hash in enumerate(hashes)]
 
 
 @pytest.mark.parametrize("alignbuddy,next_hash", hashes)
-def test_codon_alignment(alignbuddy, next_hash):
+def test_codon_alignment1(alignbuddy, next_hash):
     tester = Alb.codon_alignment(alignbuddy)
     assert align_to_hash(tester) == next_hash
 
@@ -300,10 +295,18 @@ def test_codon_alignment(alignbuddy, next_hash):
             Alb.codon_alignment(alb_objects[8])
 
 
-# ###########################################  'cs', '--clean_seqs' ############################################ #
+def test_codon_alignment2():
+    with pytest.raises(TypeError):
+        tester = Alb._make_copies(alb_objects[3])
+        tester.alignments[0][0].seq = Seq("MLDILSKFKGVTPFKGITIDDGWDQLNRSFMFVLLVVMGTTVTVRQYTGSVISCDGFKKFGSTFAEDYCWTQGLY",
+                                          alphabet=IUPAC.protein)
+        Alb.translate_cds(tester)
+
+
+# ##############################################  'cs', '--clean_seqs' ############################################### #
 def test_clean_seqs():
     # Test an amino acid file
-    tester = make_copies(alb_objects[8])
+    tester = Alb._make_copies(alb_objects[8])
     Alb.clean_seq(tester)
 
     with pytest.raises(ValueError):
@@ -317,10 +320,50 @@ def test_clean_seqs():
     assert align_to_hash(tester) == "b613060f43f66248cda0088c06de0949"
 
     # Test nucleotide files, but skip the errors
-    tester = make_copies(alb_objects[11])
+    tester = Alb._make_copies(alb_objects[11])
     Alb.clean_seq(tester, skip_list="RYWSMKHBVDNXrywsmkhbvdnx")
     tester.out_format = "genbank"
     assert align_to_hash(tester) == "81189bf7962cc2664815dc7fca8cd95d"
 
     Alb.clean_seq(tester)
     assert align_to_hash(tester) == "2f2cbf227d45aa49d15971ce214d3191"
+
+# ###########################################  'tr', '--translate' ############################################ #
+hashes = ["fa915eafb9eb0bfa0ed8563f0fdf0ef9", "5064c1d6ae6192a829972b7ec0f129ed", "ce423d5b99d5917fbef6f3b47df40513",
+          "2340addad40e714268d2523cdb17a78c", "6c66f5f63c5fb98f5855fb1c847486ad", "d9527fe1dfd2ea639df267bb8ee836f7"]
+hashes = [(Alb._make_copies(alb_objects[nucl_indices[indx]]), next_hash) for indx, next_hash in enumerate(hashes)]
+
+
+@pytest.mark.parametrize("alignbuddy,next_hash", hashes)
+def test_translate1(alignbuddy, next_hash, capsys):
+    tester = Alb.translate_cds(alignbuddy)
+    assert align_to_hash(tester) == next_hash
+
+    if next_hash == "6c66f5f63c5fb98f5855fb1c847486ad":
+        out, err = capsys.readouterr()
+        assert err == "Warning: First codon 'GGT' is not a start codon in Ael_PanxβQ___CDS\n"
+
+
+def test_translate2():
+    # Protein input
+    with pytest.raises(TypeError):
+        Alb.translate_cds(alb_objects[4])
+
+    # Non-standard length
+    tester = Alb._make_copies(alb_objects[0])
+    tester.alignments[0][0].seq = Seq(re.sub("-$", "t", str(tester.alignments[0][0].seq)),
+                                      alphabet=tester.alignments[0][0].seq.alphabet)
+    Alb.translate_cds(tester)
+    assert align_to_hash(tester) == "fa915eafb9eb0bfa0ed8563f0fdf0ef9"
+
+    # Final codon not stop and stop codon not at end of seq
+    tester = Alb._make_copies(alb_objects[0])
+    tester.alignments[0][0].seq = Seq(re.sub("---$", "ttt", str(tester.alignments[0][0].seq)),
+                                      alphabet=tester.alignments[0][0].seq.alphabet)
+    Alb.translate_cds(tester)
+    assert align_to_hash(tester) == "2ca5b0bac98226ee6a53e17503f12197"
+
+    # Non-standard codon
+    tester = Alb.AlignBuddy(resource("ambiguous_dna_alignment.fa"))
+    Alb.translate_cds(tester)
+    assert align_to_hash(tester) == "ab8fb45a38a6e5d553a29f3613bbc1a1"
