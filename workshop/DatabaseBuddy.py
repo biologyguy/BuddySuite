c42e0bee1d808991b77d5346b82bc6742a915902
biologyguy <biologyguy@gmail.com>
2015-07-16 10:46:44 -0400

    Initial implementation of DatabaseBuddy core structure
    - Build up a similar core structure as other BuddySuite tools
        - Wish List
        - API internal/helper functions
        - Db Buddy class and helper functions
        - API functions
        - Command line UI initialization
        - Command line logic
    - Initial unit test file (not functional, but structure is there)

--- workshop/DatabaseBuddy.py
+++ workshop/DatabaseBuddy.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Created on: Jan 15 2015 
 
 """
-DESCRIPTION OF PROGRAM
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+License as published by the Free Software Foundation, version 2 of the License (GPLv2).
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details at http://www.gnu.org/licenses/.
+
+name: DbBuddy.py
+date: July-16-2015
+version: 1, unstable
+author: Stephen R. Bond
+email: steve.bond@nih.gov
+institute: Computational and Statistical Genomics Branch, Division of Intramural Research,
+           National Human Genome Research Institute, National Institutes of Health
+           Bethesda, MD
+repository: https://github.com/biologyguy/BuddySuite
+© license: Gnu General Public License, Version 2.0 (http://www.gnu.org/licenses/gpl.html)
+derivative work: No
+
+Description:
+Collection of functions that interact with public sequence databases. Pull them into a script or run from command line.
 """
 
 import sys
-import os
-import re
-import shutil
-
-import argparse
 
 
 # ##################################################### WISH LIST #################################################### #
@@ -20,42 +34,106 @@ def get_genbank_file():
     return x
 
 
-def run_prosite():
-    x = 1
-    return x
+# ################################################# HELPER FUNCTIONS ################################################# #
+class GuessError(Exception):
+    """Raised when input format cannot be guessed"""
+    def __init__(self, _value):
+        self.value = _value
+
+    def __str__(self):
+        return self.value
 
 
-parser = argparse.ArgumentParser(prog="DatabaseBuddy", description="", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-parser.add_argument("positional_arg1", help="", action="store")
-parser.add_argument("-t", "--true", help="", action="store_true", default=False)
-parser.add_argument("-c", "--choice", help="", type=str, choices=["", ""], default=False)
-parser.add_argument("-m", "--multi_arg", nargs="+", help="", default=[])
-
-in_args = parser.parse_args()
+def _stderr(message, quiet=False):
+    if not quiet:
+        sys.stderr.write(message)
+    return
 
 
-class NewClass():
-    """DESCRIPTION OF CLASS"""
-    def __init__(self):
-        self.x = 1
-
-    def class_def(self):
-        self.x = 1
-        return self.x
+def _stdout(message, quiet=False):
+    if not quiet:
+        sys.stdout.write(message)
+    return
 
 
-def def1():
-    """DESCRIPTION OF FUNC"""
-    x = 1
-    return x
+# ##################################################### DB BUDDY ##################################################### #
+class DbBuddy:  # Open a file or read a handle and parse, or convert raw into a Seq object
+    def __init__(self, _input, _in_format=None, _out_format=None):
+        # ####  IN AND OUT FORMATS  #### #
+        # Holders for input type. Used for some error handling below
+        in_handle = None
+        _raw_search = None
+        in_file = None
 
 
-def def2():
-    """DESCRIPTION OF FUNC"""
-    x = 1
-    return x
+def guess_database(_input):  # _input can be list, SeqBuddy object, file handle, or file path.
+    # If input is just a list, there is no BioPython in-format. Default to gb.
+    if isinstance(_input, list):
+        return "gb"
+# #################################################################################################################### #
 
 
+def search_ensembl(_dbbuddy):
+    return _dbbuddy
+
+
+def download_everything(_dbbuddy):
+    return _dbbuddy
+
+# ################################################# COMMAND LINE UI ################################################## #
 if __name__ == '__main__':
-    print('Hello')
+    import argparse
+    parser = argparse.ArgumentParser(prog="DbBuddy.py", formatter_class=argparse.RawTextHelpFormatter,
+                                     description="Commandline wrapper for all the fun functions in this file. "
+                                                 "Access those databases!")
+
+    parser.add_argument("user_input", help="Specify accession numbers or search terms, either in a file or as space "
+                                           "separated list", nargs="*", default=[sys.stdin])
+
+    parser.add_argument('-v', '--version', action='version',
+                        version='''\
+DbBuddy 1.alpha (2015)
+
+Gnu General Public License, Version 2.0 (http://www.gnu.org/licenses/gpl.html)
+This is free software; see the source for detailed copying conditions.
+There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.
+Questions/comments/concerns can be directed to Steve Bond, steve.bond@nih.gov''')
+
+    parser.add_argument('-de', '--download_everything',
+                        help="Retrieve full records for all search terms and accessions")
+    parser.add_argument('-q', '--quiet', help="Suppress stderr messages", action='store_true')
+    parser.add_argument('-t', '--test', action='store_true',
+                        help="Run the function and return any stderr/stdout other than sequences.")
+    parser.add_argument('-o', '--out_format', help="If you want a specific format output", action='store')
+    parser.add_argument('-f', '--in_format', help="If SeqBuddy can't guess the file format, just specify it directly.",
+                        action='store')
+
+    in_args = parser.parse_args()
+
+    dbbuddy = []
+    search_set = ""
+
+    try:
+        for search_set in in_args.sequence:
+            search_set = DbBuddy(search_set, in_args.in_format, in_args.out_format)
+            dbbuddy += search_set.records
+
+        seqbuddy = DbBuddy(dbbuddy, search_set.in_format, search_set.out_format)
+    except GuessError:
+        sys.exit("Error: SeqBuddy could not understand your input. "
+                 "Check the file path or try specifying an input type with -f")
+
+    # ############################################# INTERNAL FUNCTION ################################################ #
+    def _print_recs(_seqbuddy):
+        if in_args.test:
+            _stderr("*** Test passed ***\n", in_args.quiet)
+            pass
+
+        else:
+            _stdout("{0}\n".format(str(_seqbuddy).rstrip()))
+
+    # ############################################## COMMAND LINE LOGIC ############################################## #
+    # Download everything
+    if in_args.download_everything:
+        _print_recs(download_everything(dbbuddy))
---
+++ workshop/dbbuddy_test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pytest
+import os
+from hashlib import md5
+
+
+try:
+    import workshop.DbBuddy as Db
+except ImportError:
+    import DbBuddy as Db
+
+
+def result_to_hash(_dbbuddy, mode='hash'):
+    if mode != "hash":
+        return str(_dbbuddy)
+    _hash = md5("{0}\n".format(str(_dbbuddy).rstrip()).encode()).hexdigest()
+    return _hash
+
+root_dir = os.getcwd()
+
+
+def resource(file_name):
+    return "{0}/unit_test_resources/{1}".format(root_dir, file_name)
+
+
+accession_files = ["genbank.csv", "genbank.tsv"]
+accession_types = ["genbank", "genbank"]
+
+input_tuples = [(next_file, accession_types[indx]) for indx, next_file in enumerate(accession_files)]
+
+
+@pytest.mark.parametrize("align_file,file_type", input_tuples)
+def test_instantiate_dbbuddy_from_file(align_file, file_type):
+    assert type(Db.DbBuddy(resource(align_file), _in_format=file_type)) == Db.DbBuddy
