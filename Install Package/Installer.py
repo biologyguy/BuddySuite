e9aa7530f6a9bf635facf1eda4a7bf07b2c89862
keatke <karlkeat@gmail.com>
2015-07-28 13:23:17 -0400

    Fix error when /buddysuite does not exist

--- Install Package/Installer.py
+++ Install Package/Installer.py
@@ -104,7 +104,7 @@ class BuddyInstall:
             if path.exists("/usr/local/bin/buddysuite/{0}".format(loc)):
                 os.remove("/usr/local/bin/buddysuite/{0}".format(loc))
 
-        if os.listdir("/usr/local/bin/buddysuite") is []:
+        if len(os.listdir("/usr/local/bin/buddysuite")) == 0:
             shutil.rmtree(path.realpath("/usr/local/bin/buddysuite"))
             os.remove("/usr/local/bin/buddysuite")
 
