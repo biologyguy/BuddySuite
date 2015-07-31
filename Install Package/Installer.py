a9076332f8c161af91dc49218a6f6410ab2f5f5e
keatke <karlkeat@gmail.com>
2015-07-31 16:05:32 -0400

    Fix .profile being overwritten

--- Install Package/Installer.py
+++ Install Package/Installer.py
@@ -253,13 +253,24 @@ class BuddyInstall:
 
     @staticmethod
     def edit_profile():
-        make_file = open("{0}/.bashrc".format(home_dir), 'w')
-        make_file.close()
-        with open("{0}/.bashrc".format(home_dir)) as file:
+        if not path.exists("{0}/.profile".format(home_dir)):
+            make_file = open("{0}/.profile".format(home_dir), 'w')
+            make_file.close()
+        with open("{0}/.profile".format(home_dir)) as file:
+            if 'export PATH=$PATH:{0}/buddysuite/'.format(home_dir) not in file.read():
+                file.close()
+                with open("{0}/.profile".format(home_dir), 'a') as file_write:
+                    file_write.write("\n\n# added by BuddySuite installer\n")
+                    file_write.write('export PATH=$PATH:{0}/buddysuite\n'.format(home_dir))
+
+        if not path.exists("{0}/.bashrc".format(home_dir)):
+            make_file = open("{0}/.bashrc".format(home_dir), 'w')
+            make_file.close()
+        with open("{0}/.profile".format(home_dir)) as file:
             if 'export PATH=$PATH:{0}/buddysuite/'.format(home_dir) not in file.read():
                 file.close()
                 with open("{0}/.bashrc".format(home_dir), 'a') as file_write:
-                    file_write.write("# added by BuddySuite installer\n")
+                    file_write.write("\n\n# added by BuddySuite installer\n")
                     file_write.write('export PATH=$PATH:{0}/buddysuite\n'.format(home_dir))
 
 
