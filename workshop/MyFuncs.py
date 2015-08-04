1ff075e0dd6e7336a4aaf4abeff71bdc02495911
biologyguy <biologyguy@gmail.com>
2015-08-04 09:57:14 -0400

    Update to most recent version of MyFuncs

--- workshop/MyFuncs.py
+++ workshop/MyFuncs.py
@@ -3,20 +3,38 @@
 from multiprocessing import Process, cpu_count
 from sys import stdout, exit, stderr
 from time import time
-from math import floor
+from math import floor, ceil
 import os
 from tempfile import TemporaryDirectory
 from shutil import copytree, rmtree
 from re import sub
+import string
+from random import choice
+
+
+class Timer:
+    def __init__(self):
+        self.current_time = round(time())
+
+    def start(self):
+        self.current_time = round(time())
+        return
+
+    def end(self):
+        return pretty_time(round(time()) - self.current_time)
 
 
 # maybe use curses library in the future to extend this for multi-line printing
-class DynamicPrint():
-    def __init__(self, out_type=stdout):
+class DynamicPrint:
+    def __init__(self, out_type="stdout", quiet=False):
         self._last_print = ""
         self._next_print = ""
         self._writer = self._write()
+
+        out_type = stdout if out_type == "stdout" else out_type
+        out_type = stderr if out_type == "stderr" else out_type
         self.out_type = out_type
+        self.quiet = quiet
 
     def _write(self):
         try:
@@ -29,9 +47,18 @@ class DynamicPrint():
             self.out_type.write("")
 
     def write(self, content):
-        content = sub("\t", "    ", content)
-        self._next_print = content
-        next(self._writer)
+        if not self.quiet:
+            content = sub("\t", "    ", content)
+            self._next_print = content
+            next(self._writer)
+        return
+
+    def new_line(self):
+        if not self.quiet:
+            self.out_type.write("\n")
+            self.out_type.flush()
+            self._last_print = ""
+        return
 
 
 def pretty_time(seconds):
@@ -59,23 +86,42 @@ def pretty_time(seconds):
     return output
 
 
+def pretty_number(num):
+    magnitude = 0
+    while abs(num) >= 1000 and magnitude < 11:
+        magnitude += 1
+        num /= 1000.0
+    return '%.2f %s' % (num, ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y', 'X', 'S', 'D'][magnitude])
+
+
+def usable_cpu_count():
+    cpus = cpu_count()
+    if cpus > 7:
+        max_processes = cpus - 3
+    elif cpus > 3:
+        max_processes = cpus - 2
+    elif cpus > 1:
+        max_processes = cpus - 1
+    else:
+        max_processes = 1
+
+    return max_processes
+
+
 def run_multicore_function(iterable, function, func_args=False, max_processes=0, quiet=False, out_type=stdout):
         # fun little piece of abstraction here... directly pass in a function that is going to be looped over, and
         # fork those loops onto independent processes. Any arguments the function needs must be provided as a list.
         d_print = DynamicPrint(out_type)
-        cpus = cpu_count()
-        if max_processes == 0:
-            if cpus > 7:
-                max_processes = cpus - 3
-            elif cpus > 3:
-                max_processes = cpus - 2
-            elif cpus > 1:
-                max_processes = cpus - 1
-            else:
-                max_processes = 1
 
-        if max_processes > cpus:
-            max_processes = cpus
+        if max_processes == 0:
+            max_processes = usable_cpu_count()
+
+        else:
+            cpus = cpu_count()
+            if max_processes > cpus:
+                max_processes = cpus
+            elif max_processes < 1:
+                max_processes = 1
 
         running_processes = 0
         child_list = []
@@ -154,17 +200,35 @@ def run_multicore_function(iterable, function, func_args=False, max_processes=0,
         return
 
 
-class TempDir():
+class TempDir:
     def __init__(self):
         self.dir = next(self._make_dir())
         self.path = self.dir.name
+        self.subdirs = []
 
     def _make_dir(self):
         tmp_dir = TemporaryDirectory()
         yield tmp_dir
         rmtree(self.path)
 
-    def save(self, location):
+    def subdir(self):
+        new_hash = "".join([choice(string.ascii_letters + string.digits) for _ in range(10)])
+        while new_hash in self.subdirs:  # Catch the very unlikely case that a duplicate occurs
+            new_hash = "".join([choice(string.ascii_letters + string.digits) for _ in range(10)])
+
+        subdir_path = "%s/%s" % (self.path, new_hash)
+        os.mkdir(subdir_path)
+        self.subdirs.append(new_hash)
+        return subdir_path
+
+    def del_subdir(self, _dir):
+        _dir = _dir.split("/")[-1]
+        del self.subdirs[self.subdirs.index(_dir)]
+        rmtree("%s/%s" % (self.path, _dir))
+        return
+
+    def save(self, location, keep_hash=False):
+        location = location if not keep_hash else "%s/%s" % (location, self.path.split("/")[-1])
         if os.path.isdir(location):
             print("Save Error: Indicated output folder already exists in TempDir.save(%s)" % location, file=stderr)
             return False
@@ -173,7 +237,7 @@ class TempDir():
             return True
 
 
-class TempFile():
+class TempFile:
     # I really don't like the behavior of tempfile.[Named]TemporaryFile(), so hack TemporaryDirectory() via TempDir()
     def __init__(self):
         self._tmp_dir = TempDir()  # This needs to be a persistent (ie self.) variable, or the directory will be deleted
@@ -225,7 +289,7 @@ class TempFile():
         return
 
 
-class SafetyValve():  # Use this class if you're afraid of an infinit loop
+class SafetyValve:  # Use this class if you're afraid of an infinit loop
     def __init__(self, global_reps=1000, state_reps=10, counter=0):
         self.counter = counter
         
@@ -253,7 +317,6 @@ class SafetyValve():  # Use this class if you're afraid of an infinit loop
         if self.state_reps == 0:
             exit("Error: You just popped your state_reps safety valve. %s" % message)
 
-
 # Pulled this function off of Stack Overflow -- posted by nosklo
 # Note that this is a generator, so need to use next() or `with` to get a result
 def walklevel(some_dir, level=1):
@@ -265,3 +328,37 @@ def walklevel(some_dir, level=1):
         num_sep_this = root.count(os.path.sep)
         if num_sep + level <= num_sep_this:
             del dirs[:]
+
+
+def normalize(data, trim_ends=1.0):
+    if 0. > trim_ends > 1.0:
+        raise ValueError("normalize() trim_ends parameter should be between 0.5 and 1.0")
+
+    if trim_ends > 0.5:
+        trim_ends = 1 - trim_ends
+
+    max_limit = ceil(len(data) * (1 - trim_ends)) - 1
+    min_limit = -1 * (max_limit + 1)
+
+    if type(data) == dict:
+        sorted_data = sorted([data[key] for key in data])
+        _max = sorted_data[max_limit]
+        _min = sorted_data[min_limit]
+        data_range = _max - _min
+        for key in data:
+            data[key] = (data[key] - _min) / data_range
+            data[key] = 1. if data[key] > 1. else data[key]
+            data[key] = 0. if data[key] < 0. else data[key]
+
+    else:
+        sorted_data = sorted(data)
+        _max = sorted_data[max_limit]
+        _min = sorted_data[min_limit]
+        data_range = _max - _min
+        for i in range(len(data)):
+            data[i] = (data[i] - _min) / data_range
+            data[i] = 1. if data[i] > 1. else data[i]
+            data[i] = 0. if data[i] < 0. else data[i]
+
+    return data
+
