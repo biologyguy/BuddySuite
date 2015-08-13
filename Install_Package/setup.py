75bb7a5518a9e7a45af2f7abffa0baf76c9c5de0
keatke <karlkeat@gmail.com>
2015-08-13 13:53:41 -0400

    Add more pypi stuff

--- Install_Package/setup.py
+++ Install_Package/setup.py
@@ -7,7 +7,7 @@ setup(
     author = 'Stephen Bond',
     author_email = 'biologyguy@gmail.com',
     url = 'https://github.com/biologyguy/BuddySuite',
-    download_url = 'https://github.com/biologyguy/BuddySuite/tarball/0.1', # git tag 0.1 -m "Adds a tag so that we can put this on PyPI."
+    download_url = 'https://github.com/biologyguy/BuddySuite/tarball/1.alpha', # git tag 1.alpha -m "Adds a tag so that we can put this on PyPI."
     keywords = ['testing', 'logging', 'example'], # arbitrary keywords
     classifiers = [],
 )
