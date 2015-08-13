fec27edbdfd016cdc12a177aaa1bae87ac972646
keatke <karlkeat@gmail.com>
2015-08-13 15:33:31 -0400

    Add more pypi stuff

--- Install_Package/setup.py
+++ Install_Package/setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-    name = 'BuddySuite',
-    packages = ['BuddySuite'], # this must be the same as the name above
-    version = '1.alpha',
+    name = 'SeqBuddy',
+    packages = ['SeqBuddy'], # this must be the same as the name above
+    version = 'v1.0',
     description = 'A python toolkit for working with biological data',
     author = 'Stephen Bond',
     author_email = 'biologyguy@gmail.com',
     url = 'https://github.com/biologyguy/BuddySuite',
-    download_url = 'https://github.com/biologyguy/BuddySuite/tarball/1.alpha', # git tag 1.alpha -m "Adds a tag so that we can put this on PyPI."
+    download_url = 'https://github.com/biologyguy/BuddySuite/archive/SeqBuddy_v1.0.tar.gz', # git tag 1.alpha -m "Adds a tag so that we can put this on PyPI."
     keywords = ['testing', 'logging', 'example'], # arbitrary keywords
     classifiers = [],
 )
