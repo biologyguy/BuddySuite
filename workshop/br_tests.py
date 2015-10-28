6d4d9423dc63815a658066786618f4547b186fa5
biologyguy <biologyguy@gmail.com>
2015-10-28 12:40:54 -0400

    Pushing the current (broken) state of tests
    I just want this committed, even though it doesn't work. The framework is more or less ready to be filled in.

--- workshop/br_tests.py
+++ workshop/br_tests.py
@@ -26,29 +26,94 @@ Description: Collection of PyTest unit tests for the buddy_resources.py module
 import pytest
 from subprocess import Popen, PIPE
 
-try:
-    import workshop.buddy_resources as br
-except ImportError:
-    import buddy_resources as br
+import buddy_resources as Br
 
 
 def test_versions():
     sb_ver = Popen("sb -v", stdout=PIPE, shell=True).communicate()
-    assert sb_ver[0].decode() == str(br.VERSIONS["SeqBuddy"])
+    assert sb_ver[0].decode() == str(Br.VERSIONS["SeqBuddy"])
 
     alb_ver = Popen("alb -v", stdout=PIPE, shell=True).communicate()
-    assert alb_ver[0].decode() == str(br.VERSIONS["AlignBuddy"])
+    assert alb_ver[0].decode() == str(Br.VERSIONS["AlignBuddy"])
 
     pb_ver = Popen("pb -v", stdout=PIPE, shell=True).communicate()
-    assert pb_ver[0].decode() == str(br.VERSIONS["PhyloBuddy"])
+    assert pb_ver[0].decode() == str(Br.VERSIONS["PhyloBuddy"])
 
     db_ver = Popen("db -v", stdout=PIPE, shell=True).communicate()
-    assert db_ver[0].decode() == str(br.VERSIONS["DatabaseBuddy"])
+    assert db_ver[0].decode() == str(Br.VERSIONS["DatabaseBuddy"])
 
 
 # ######################  '_format_to_extension' ###################### #
 def test_format_to_extension():
-    ext_dict = {'fasta': 'fa', 'fa': 'fa', 'genbank': 'gb', 'gb': 'gb', 'nexus': 'nex', 'nex': 'nex', 'phylip': 'phy',
-                'phy': 'phy', 'phylip-relaxed': 'phyr', 'phyr': 'phyr', 'stockholm': 'stklm', 'stklm': 'stklm'}
+    ext_dict = {'fasta': 'fa', 'fa': 'fa', 'genbank': 'gb', 'gb': 'gb', 'newick': 'nwk', 'nwk': 'nwk', 'nexus': 'nex',
+                'nex': 'nex', 'phylip': 'phy', 'phy': 'phy', 'phylip-relaxed': 'phyr', 'phyr': 'phyr',
+                'phylipss': 'physs', 'physs': 'physs', 'phylipsr': 'physr', 'physr': 'physr', 'stockholm': 'stklm',
+                'stklm': 'stklm'}
     for i, j in ext_dict.items():
-        assert j == br.format_to_extension(i)
+        assert j == Br.format_to_extension(i)
+
+
+def testphylip_sequential_out():
+    tester = alignments.get_one("m p py")
+    output = Br.phylip_sequential_out(tester, relaxed=False)
+    assert string2hash("%s\n" % output.rstrip()) == "eb82cda31fcb2cf00e11d7e910fde695"
+
+    tester = alignments.get_one("m p pr")
+    output = Br.phylip_sequential_out(tester, relaxed=True)
+    assert string2hash("%s\n" % output.rstrip()) == "a16c6e617e5a88fef080eea54e54e8a8"
+
+    records = tester.records()
+    with pytest.raises(br.PhylipError) as e:
+        records[0].id = str(records[1].id)
+        Br.phylip_sequential_out(tester)
+    assert "Malformed Phylip --> Repeat id" in str(e)
+
+    with pytest.raises(br.PhylipError) as e:
+        records[1].id = "foo"
+        records[0].seq = Seq(str(records[0].seq[:-2]), records[0].seq.alphabet)
+        Br.phylip_sequential_out(tester)
+    assert "Malformed Phylip --> The length of record 'foo' is incorrect" in str(e)
+
+    with pytest.raises(br.PhylipError) as e:
+        records[0].seq = Seq("AT%s" % records[0].seq, records[0].seq.alphabet)
+        Br.phylip_sequential_out(tester, relaxed=False)
+    assert "Malformed Phylip --> Repeat id" in str(e) and "after strict truncation." in str(e)
+
+
+def testphylip_sequential_read():
+    tester = alignments.get_one("m p pss")
+    aligns = Br.phylip_sequential_read(str(tester), relaxed=False)
+    assert len(Br.AlignBuddy(aligns).records()) == 29
+
+    with pytest.raises(br.PhylipError) as e:
+        Br.phylip_sequential_read(str(tester))
+    assert "Malformed Phylip --> 8 sequences expected, 4 found" in str(e)
+
+    tester = alignments.get_one("m p psr")
+    aligns = Br.phylip_sequential_read(str(tester))
+    assert len(Br.AlignBuddy(aligns).records()) == 34
+
+    with pytest.raises(br.PhylipError) as e:
+        with open(resource("malformed_phylip_columns.physs"), "r") as ifile:
+            Br.phylip_sequential_read(ifile.read(), relaxed=False)
+    assert "Malformed Phylip --> Less sequence found than expected" in str(e)
+
+    with pytest.raises(br.PhylipError) as e:
+        with open(resource("malformed_phylip_records_more.physs"), "r") as ifile:
+            Br.phylip_sequential_read(ifile.read(), relaxed=False)
+    assert "Malformed Phylip --> 7 sequences expected, 8 found." in str(e)
+
+    with pytest.raises(br.PhylipError) as e:
+        with open(resource("malformed_phylip_columns_more.physs"), "r") as ifile:
+            Br.phylip_sequential_read(ifile.read(), relaxed=False)
+    assert "Malformed Phylip --> Sequence Mle-Panxα2 has 2046 columns, 2043 expected." in str(e)
+
+    with pytest.raises(br.PhylipError) as e:
+        with open(resource("malformed_phylip_repeat_id.physr"), "r") as ifile:
+            Br.phylip_sequential_read(ifile.read(), relaxed=True)
+    assert "Malformed Phylip --> Repeat ID Mle-Pxα1." in str(e)
+
+    with pytest.raises(br.PhylipError) as e:
+        with open(resource("malformed_phylip_repeat_id.physr"), "r") as ifile:
+            Br.phylip_sequential_read(ifile.read(), relaxed=False)
+    assert "Malformed Phylip --> Repeat id 'Mle-Pxα1  ' after strict truncation." in str(e)
