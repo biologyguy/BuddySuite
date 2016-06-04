1fa02e939d19704be138628cb596c34e03cd81bc
Steve Bond <biologyguy@gmail.com>
2016-06-04 12:26:31 -0700

    Initial buddy_resources test script

---
+++ buddysuite/tests/test_buddy_resources/test_buddy_resources.py
@@ -0,0 +1,23 @@
+#!/use/bin/env python3
+# coding=utf-8
+
+import buddysuite.buddy_resources as br
+import pytest
+
+
+def test_parse_format():
+    for _format in ["phylip", "phylipis", "phylip-strict", "phylip-interleaved-strict"]:
+        assert br.parse_format(_format) == "phylip"
+
+    for _format in ["phylipi", "phylip-relaxed", "phylip-interleaved", "phylipr"]:
+        assert br.parse_format(_format) == "phylip-relaxed"
+
+    for _format in ["phylips", "phylipsr", "phylip-sequential", "phylip-sequential-relaxed"]:
+        assert br.parse_format(_format) == "phylipsr"
+
+    for _format in ["phylipss", "phylip-sequential-strict"]:
+        assert br.parse_format(_format) == "phylipss"
+
+    with pytest.raises(TypeError) as e:
+        br.parse_format("foo")
+    assert "Format type 'foo' is not recognized/supported" in str(e)
