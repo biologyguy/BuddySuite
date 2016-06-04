1550eb254dd2fec191d7c29bad9ad2a023a76d16
Steve Bond <biologyguy@gmail.com>
2016-06-04 11:42:51 -0700

    Switch the docker pytest script to point at the new tests directory
     This should stop the build from failing, and give us a cleaner slate to work from

--- buddysuite-test/test.sh
+++ buddysuite-test/test.sh
@@ -5,8 +5,13 @@ source activate test-environment
 
 TEST_SCRIPTS=()
 #TEST_SCRIPTS+=('alignbuddy_test.py')
-TEST_SCRIPTS+=('phylobuddy_test.py')
+#TEST_SCRIPTS+=('phylobuddy_test.py')
 #TEST_SCRIPTS+=('sequddy_test.py')
+TEST_SCRIPTS+=('tests/test_fixtures.py')
+TEST_SCRIPTS+=('tests/test_buddy_resources/test_buddy_resources.py')
+TEST_SCRIPTS+=('tests/test_align_buddy/test_alb_class_and_helpers.py')
+TEST_SCRIPTS+=('tests/test_align_buddy/test_alb_api.py')
+TEST_SCRIPTS+=('tests/test_align_buddy/test_alb_ui.py')
 
 cd ~/buddysuite/workshop
 # disable cacheprovider since it requires r/w access to the test directory
