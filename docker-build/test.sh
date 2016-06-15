8a7da0dfba904c796ae21e9111dd6cf8f708835c
biologyguy <biologyguy@gmail.com>
2016-06-15 13:24:37 -0400

    Remove the creation of a Conda virtual env in Docker, and swap update order
    There was an issue with conda in the Docker container relating back to when the bioconda channel was added compared to when conda is updated.
    By switching the channel before updating, the issue appears to be resolved.
    I've also removed the conda virtual environment because it's unnecessary. It's just adding another level of complexity without adding any value.

--- docker-build/test.sh
+++ docker-build/test.sh
@@ -2,25 +2,24 @@
 # -*- coding: utf-8 -*-
 
 export PATH="$HOME/miniconda/bin:$PATH"
-source activate test-environment
 
 cd /home/docker/BuddySuite/buddysuite/tests
-find . -name "__pycache__" -type d | xargs rm -r || echo "No pycache detected"
 
-#sudo git pull
+# py.test caches things, which may be incompatible with the Docker environment. Delete any caches before proceeding
+find . -name "__pycache__" -type d | xargs rm -r || echo "No pycache detected"
 
 # Disable py.test cacheprovider because it requires r/w access to the test directory
 #### Pre-tests
 TEST_SCRIPTS='test_fixtures.py '
 py.test ${TEST_SCRIPTS} --cache-clear -p no:cacheprovider
 
-cd /home/docker/BuddySuite/buddysuite/tests/test_buddy_resources
 #### Buddy Resources
+cd /home/docker/BuddySuite/buddysuite/tests/test_buddy_resources
 TEST_SCRIPTS='test_buddy_resources.py '
 py.test ${TEST_SCRIPTS} --cache-clear -p no:cacheprovider
 
-cd /home/docker/BuddySuite/buddysuite/tests/test_align_buddy
 #### AlignBuddy
+cd /home/docker/BuddySuite/buddysuite/tests/test_align_buddy
 TEST_SCRIPTS='test_alb_class_and_helpers.py '
 TEST_SCRIPTS+='test_alb_api.py '
 TEST_SCRIPTS+='test_alb_ui.py '
