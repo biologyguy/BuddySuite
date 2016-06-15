70165afdbe75c0b0129ff5358eb7672325944c33
biologyguy <biologyguy@gmail.com>
2016-06-15 15:59:12 -0400

    Install a bunch of 3rd party AlignBuddy dependencies into Dockerfile
    MAFFT
    prank
    pagan
    Muscle
    ClustalO
    ClustalW

--- docker-build/test.sh
+++ docker-build/test.sh
@@ -11,16 +11,17 @@ find . -name "__pycache__" -type d | xargs rm -r || echo "No pycache detected"
 # Disable py.test cacheprovider because it requires r/w access to the test directory
 #### Pre-tests
 TEST_SCRIPTS='test_fixtures.py '
-py.test ${TEST_SCRIPTS} --cache-clear -p no:cacheprovider
+#py.test ${TEST_SCRIPTS} --cache-clear -p no:cacheprovider
 
 #### Buddy Resources
 cd /home/docker/BuddySuite/buddysuite/tests/test_buddy_resources
 TEST_SCRIPTS='test_buddy_resources.py '
-py.test ${TEST_SCRIPTS} --cache-clear -p no:cacheprovider
+#py.test ${TEST_SCRIPTS} --cache-clear -p no:cacheprovider
 
 #### AlignBuddy
 cd /home/docker/BuddySuite/buddysuite/tests/test_align_buddy
 TEST_SCRIPTS='test_alb_class_and_helpers.py '
 TEST_SCRIPTS+='test_alb_api.py '
 TEST_SCRIPTS+='test_alb_ui.py '
+#TEST_SCRIPTS+='test_alb_3rd_party.py '
 py.test ${TEST_SCRIPTS} --cache-clear -p no:cacheprovider
