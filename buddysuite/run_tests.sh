9a3e7cb037d051d42bf759845fa3c91ae6434548
biologyguy <biologyguy@gmail.com>
2016-07-12 13:21:44 -0400

    Append command line args to run_tests.sh
    This will allow throwing -m flag when running rests (to suppress 3rd party tests, for example)

--- buddysuite/run_tests.sh
+++ buddysuite/run_tests.sh
@@ -5,16 +5,16 @@ find . -name "__pycache__" -type d | xargs rm -r || echo "No pycache detected"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "test_fixtures.py"
-py.test ${DIR}/tests/test_fixtures.py  --cov ${DIR}/tests/conftest.py --cov-report html -p no:cacheprovider
+py.test ${DIR}/tests/test_fixtures.py  --cov ${DIR}/tests/conftest.py --cov-report html -p no:cacheprovider $@
 
 echo "buddy_resources.py"
-py.test ${DIR}/tests/test_buddy_resources/ --cov ${DIR}/buddy_resources.py --cov-report html -p no:cacheprovider
+py.test ${DIR}/tests/test_buddy_resources/ --cov ${DIR}/buddy_resources.py --cov-report html -p no:cacheprovider $@
 
 echo "AlignBuddy.py"
-py.test ${DIR}/tests/test_alignbuddy/ --cov ${DIR}/AlignBuddy.py --cov-report html -n 8 -p no:cacheprovider
+py.test ${DIR}/tests/test_alignbuddy/ --cov ${DIR}/AlignBuddy.py --cov-report html -n 8 -p no:cacheprovider $@
 
 echo "SeqBuddy.py"
-py.test ${DIR}/tests/test_seqbuddy/ --cov ${DIR}/SeqBuddy.py --cov-report html -n 8 -p no:cacheprovider
+py.test ${DIR}/tests/test_seqbuddy/ --cov ${DIR}/SeqBuddy.py --cov-report html -n 8 -p no:cacheprovider $@
 
 echo "PhyloBuddy.py"
-py.test ${DIR}/tests/test_phylobuddy/ --cov ${DIR}/PhyloBuddy.py --cov-report html -n 8 -p no:cacheprovider
+py.test ${DIR}/tests/test_phylobuddy/ --cov ${DIR}/PhyloBuddy.py --cov-report html -n 8 -p no:cacheprovider $@
