8e02fad388c19d807bcb9f5e0031d72bb82d171d
biologyguy <biologyguy@gmail.com>
2016-09-01 15:16:14 -0400

    More fine grained run_tests.sh script
    Makes it easier to comment out particular tests and adds the --durations flag to highlight the slowest tests

--- buddysuite/run_tests.sh
+++ buddysuite/run_tests.sh
@@ -5,19 +5,36 @@ find . -name "__pycache__" -type d | xargs rm -r || echo "No pycache detected"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "test_fixtures.py"
-py.test ${DIR}/tests/test_fixtures.py  --cov ${DIR}/tests/conftest.py --cov-report html -p no:cacheprovider $@
+TEST_SCRIPTS=${DIR}'/tests/test_fixtures.py '
+py.test ${TEST_SCRIPTS} --cov ${DIR}/tests/conftest.py --cov-report html -n 8 -p no:cacheprovider --durations=10 $@
 
 echo "buddy_resources.py"
-py.test ${DIR}/tests/test_buddy_resources/ --cov ${DIR}/buddy_resources.py --cov-report html -p no:cacheprovider $@
+TEST_SCRIPTS=${DIR}'/tests/test_buddy_resources/ '
+py.test ${TEST_SCRIPTS} --cov ${DIR}/buddy_resources.py --cov-report html -n 8 -p no:cacheprovider --durations=10 $@
 
 echo "AlignBuddy.py"
-py.test ${DIR}/tests/test_alignbuddy/ --cov ${DIR}/AlignBuddy.py --cov-report html -n 8 -p no:cacheprovider $@
+TEST_SCRIPTS=${DIR}'/tests/test_alignbuddy/test_alb_class_and_helpers.py '
+TEST_SCRIPTS+=${DIR}'/tests/test_alignbuddy/test_alb_api.py '
+TEST_SCRIPTS+=${DIR}'/tests/test_alignbuddy/test_alb_ui.py '
+#TEST_SCRIPTS+=${DIR}'/tests/test_alignbuddy/test_alb_3rd_party.py '
+py.test ${TEST_SCRIPTS} --cov ${DIR}/AlignBuddy.py --cov-report html -n 8 -p no:cacheprovider --durations=10 $@
 
 echo "DatabaseBuddy.py"
-py.test ${DIR}/tests/test_databasebuddy/ --cov ${DIR}/DatabaseBuddy.py --cov-report html -n 8 -p no:cacheprovider $@
+TEST_SCRIPTS=${DIR}'/tests/test_databasebuddy/test_db_class_and_helpers.py '
+TEST_SCRIPTS+=${DIR}'/tests/test_databasebuddy/test_db_clients.py '
+TEST_SCRIPTS+=${DIR}'/tests/test_databasebuddy/test_db_ui.py '
+py.test ${TEST_SCRIPTS} --cov ${DIR}/DatabaseBuddy.py --cov-report html -n 8 -p no:cacheprovider --durations=10 $@
 
 echo "PhyloBuddy.py"
-py.test ${DIR}/tests/test_phylobuddy/ --cov ${DIR}/PhyloBuddy.py --cov-report html -n 8 -p no:cacheprovider $@
+TEST_SCRIPTS=${DIR}'/tests/test_phylobuddy/test_pb_class_and_helpers.py '
+TEST_SCRIPTS+=${DIR}'/tests/test_phylobuddy/test_pb_api.py '
+TEST_SCRIPTS+=${DIR}'/tests/test_phylobuddy/test_pb_ui.py '
+#TEST_SCRIPTS+=${DIR}'/tests/test_phylobuddy/test_pb_3rd_party.py '
+py.test ${TEST_SCRIPTS} --cov ${DIR}/PhyloBuddy.py --cov-report html -n 8 -p no:cacheprovider --durations=10 $@
 
 echo "SeqBuddy.py"
-py.test ${DIR}/tests/test_seqbuddy/ --cov ${DIR}/SeqBuddy.py --cov-report html -n 8 -p no:cacheprovider $@
+TEST_SCRIPTS=${DIR}'/tests/test_seqbuddy/test_sb_class_and_helpers.py '
+TEST_SCRIPTS+=${DIR}'/tests/test_seqbuddy/test_sb_api.py '
+TEST_SCRIPTS+=${DIR}'/tests/test_seqbuddy/test_sb_ui.py '
+#TEST_SCRIPTS+=${DIR}'/tests/test_seqbuddy/test_sb_3rd_party.py '
+py.test ${TEST_SCRIPTS} --cov ${DIR}/SeqBuddy.py --cov-report html -n 8 -p no:cacheprovider --durations=10 $@
