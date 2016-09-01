79c2df453470bf7606bd65da1838d9ce802e2149
biologyguy <biologyguy@gmail.com>
2016-09-01 16:26:32 -0400

    Try switching .coveragerc to the 'source' directive...
    Fingers crossed

--- test.sh
+++ test.sh
@@ -2,33 +2,35 @@
 # -*- coding: utf-8 -*-
 
 export PATH="$HOME/miniconda/bin:$PATH"
-
-cd /home/travis/build/biologyguy/BuddySuite/buddysuite/tests
 FAILURE=0
 
-find . -name "__pycache__" -type d | xargs rm -r || echo "No pycache detected"
 
 # Disable py.test cacheprovider because it requires r/w access to the test directory
 #### Pre-tests
+cd /home/travis/build/biologyguy/BuddySuite/buddysuite/tests
 TEST_SCRIPTS='test_fixtures.py '
 py.test ${TEST_SCRIPTS} --cache-clear -p no:cacheprovider --cov --cov-report= --cov-config ../.coveragerc
+
 if [ $? -ne 0 ]
 then
     FAILURE=1
 fi
-cp /home/travis/build/biologyguy/BuddySuite/buddysuite/tests/conftest.py ./
-cp /home/travis/build/biologyguy/BuddySuite/buddysuite/tests/__init__.py ./
+
+#cp /home/travis/build/biologyguy/BuddySuite/buddysuite/tests/conftest.py ./
+#cp /home/travis/build/biologyguy/BuddySuite/buddysuite/tests/__init__.py ./
 mv .coverage /home/travis/build/biologyguy/BuddySuite/buddysuite/tests/test_buddy_resources/
 
 #### Buddy Resources
 cd /home/travis/build/biologyguy/BuddySuite/buddysuite/tests/test_buddy_resources
 TEST_SCRIPTS='test_buddy_resources.py '
 py.test ${TEST_SCRIPTS} --cache-clear -p no:cacheprovider --cov --cov-append --cov-report= --cov-config ../../.coveragerc
+
 if [ $? -ne 0 ]
 then
     FAILURE=1
 fi
-cp /home/travis/build/biologyguy/BuddySuite/buddysuite/buddy_resources.py ./
+
+#cp /home/travis/build/biologyguy/BuddySuite/buddysuite/buddy_resources.py ./
 mv .coverage /home/travis/build/biologyguy/BuddySuite/buddysuite/tests/test_alignbuddy/
 
 #### AlignBuddy
@@ -42,7 +44,7 @@ if [ $? -ne 0 ]
 then
     FAILURE=1
 fi
-cp /home/travis/build/biologyguy/BuddySuite/buddysuite/AlignBuddy.py ./
+#cp /home/travis/build/biologyguy/BuddySuite/buddysuite/AlignBuddy.py ./
 mv .coverage /home/travis/build/biologyguy/BuddySuite/buddysuite/tests/test_databasebuddy/
 
 #### DatabaseBuddy
@@ -55,7 +57,7 @@ if [ $? -ne 0 ]
 then
     FAILURE=1
 fi
-cp /home/travis/build/biologyguy/BuddySuite/buddysuite/DatabaseBuddy.py ./
+#cp /home/travis/build/biologyguy/BuddySuite/buddysuite/DatabaseBuddy.py ./
 mv .coverage /home/travis/build/biologyguy/BuddySuite/buddysuite/tests/test_seqbuddy/
 
 #### SeqBuddy
@@ -63,13 +65,13 @@ cd /home/travis/build/biologyguy/BuddySuite/buddysuite/tests/test_seqbuddy
 TEST_SCRIPTS='test_sb_class_and_helpers.py '
 TEST_SCRIPTS+='test_sb_api.py '
 TEST_SCRIPTS+='test_sb_ui.py '
-TEST_SCRIPTS+='test_sb_3rd_party.py '
+#TEST_SCRIPTS+='test_sb_3rd_party.py '
 py.test ${TEST_SCRIPTS} --cache-clear -p no:cacheprovider --cov --cov-append --cov-report= --cov-config ../../.coveragerc
 if [ $? -ne 0 ]
 then
     FAILURE=1
 fi
-cp /home/travis/build/biologyguy/BuddySuite/buddysuite/SeqBuddy.py ./
+#cp /home/travis/build/biologyguy/BuddySuite/buddysuite/SeqBuddy.py ./
 mv .coverage /home/travis/build/biologyguy/BuddySuite/buddysuite/tests/test_phylobuddy/
 
 #### PhyloBuddy
@@ -83,7 +85,7 @@ if [ $? -ne 0 ]
 then
     FAILURE=1
 fi
-cp /home/travis/build/biologyguy/BuddySuite/buddysuite/PhyloBuddy.py ./
+#cp /home/travis/build/biologyguy/BuddySuite/buddysuite/PhyloBuddy.py ./
 mv .coverage /home/travis/build/biologyguy/BuddySuite/
 cd /home/travis/build/biologyguy/BuddySuite
 coveralls
