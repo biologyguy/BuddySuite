4d2a820ffc0c83327243f3ad3febe43328faa2ba
biologyguy <biologyguy@gmail.com>
2016-10-05 19:06:08 -0400

    Make an empty branch for error_codes, and that's all that's ever going to live there

--- manuscript/make_manuscript.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-pdflatex bmc_article.tex;
-bibtex bmc_article;
-pdflatex bmc_article.tex;
-pdflatex bmc_article.tex;
-mv bmc_article.log bmc_article.aux bmc_article.pdf bmc_article.bbl bmc_article.blg compile_dir;
