a5f74bb4e1058033039428a0b51b4e14f39b5e95
Sofia <alicarea@gmail.com>
2016-10-20 10:00:40 -0400

    Import buddysuite modules
    This is some extra test to demonstrate body of a commit message

--- diagnostics/performance_scanner.py
+++ diagnostics/performance_scanner.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 import argparse
+from buddysuite import SeqBuddy
+from buddysuite import AlignBuddy
+from buddysuite import PhyloBuddy
+
 
 if __name__ == '__main__':
     print("Hello World\n")
