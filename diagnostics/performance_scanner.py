3784c5dffc0a602359d23ed35dfb02a538261631
Sofia <alicarea@gmail.com>
2016-10-20 10:16:58 -0400

    Add argparse to performanceScanner

--- diagnostics/performance_scanner.py
+++ diagnostics/performance_scanner.py
@@ -7,4 +7,15 @@ from buddysuite import PhyloBuddy
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog="performanceScanner", description="Check function time",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    #parser.add_argument("regex", help="pattern to search", action="store")
+    #parser.add_argument("replace", help="replacement", action="store")
+    #parser.add_argument("file", help="input file", action="store")
+    parser.add_argument("-m", "--module", help="specify a single module", action='store')
+    
+
+    in_args = parser.parse_args()
+
     print("Hello World\n")
