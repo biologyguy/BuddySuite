8f8efdd976dd674e0c2e6cf6047c54eeffba1709
Sofia <alicarea@gmail.com>
2016-10-21 10:49:36 -0400

    Add argparse to performanceScanner

--- diagnostics/performance_scanner.py
+++ diagnostics/performance_scanner.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
+import sys
 import argparse
 import timeit
 from buddysuite import SeqBuddy
+from subprocess import Popen
 from buddysuite import AlignBuddy
 from buddysuite import PhyloBuddy
-from subprocess import Popen
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog="performanceScanner", description="Check function time",
@@ -16,16 +16,10 @@ if __name__ == '__main__':
     # parser.add_argument("replace", help="replacement", action="store")
     # parser.add_argument("file", help="input file", action="store")
     # parser.add_argument("-m", "--module", help="specify a single module", action='store')
-    parser.add_argument("-f", "--file", help="specify the input file", action='store')
     parser.add_argument("-c", "--command", help="specify a single module", action='store')
     # use seq buddy for now, then ad the --module option
     in_args = parser.parse_args()
-
-    # create wrapper to use functions with arguments inside timeit:
-    def wrapper(func, *args):
-        def wrapped():
-            return func(*args)
-        return wrapped
+    # seqbuddy = SeqBuddy.SeqBuddy(All_pannexins_pep.fa)
 
     # create a dictionary with the default parameters for each function (-c, --command)
     opts_sb = {
@@ -35,8 +29,6 @@ if __name__ == '__main__':
 
     # if in_args.module == "all":
     if in_args.command == "list_ids":
-        short_list = [in_args.file, "--" + in_args.command, opts_sb[in_args.command]]
-        wrapped = wrapper(SeqBuddy, short_list)
-        timeit.timeit(wrapped, number=1000)
-        # print(timeit.timeit("sb in_args.file ", number=1000))
-        # Popen("echo Hello", shell=True).wait()
+        timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.fa -ns -q", shell=True, stderr=PIPE, stdout=PIPE).wait()', number=10)
+        newtimer = timer / 10
+        print(newtimer)
