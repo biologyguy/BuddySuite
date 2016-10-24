2fdfd6643e7bb2077b564f2aea46543d2f837560
Sofia <alicarea@gmail.com>
2016-10-24 13:27:16 -0400

    Add argparse to performanceScanner

--- diagnostics/performance_scanner.py
+++ diagnostics/performance_scanner.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     # parser.add_argument("replace", help="replacement", action="store")
     # parser.add_argument("file", help="input file", action="store")
     # parser.add_argument("-m", "--module", help="specify a single module", action='store')
-    # parser.add_argument("-c", "--command", help="specify a single module", action='store')
+    parser.add_argument("-c", "--command", help="specify a single module", action='store')
     # use seq buddy for now, then ad the --module option
     in_args = parser.parse_args()
 
@@ -19,11 +19,41 @@ if __name__ == '__main__':
     opts_sb = {
         'list_ids': '3',
         "num_seqs": "",
-        "lowercase": ""
+        "lowercase": "",
+        "ave_seq_length": "",
+        "back_translate": "",
+        "bl2seq": "",
+        "blast": "mnemiopsis_leidyi",
+        "clean_seq": "",
+        "complement": "",
+        "concat_seqs": "",
+        "count_codons": "",
     }
 
-    for flag, args in opts_sb.items():
-        sys.stdout.write("%s: " % flag)
+    if in_args.command == "all":
+        for flag, args in opts_sb.items():
+            sys.stdout.write("%s: " % flag)
+            sys.stdout.flush()
+            if flag == "bl2seq":
+                timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy seqs_25.fa --%s %s", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (flag, args), number=10)
+                sys.stdout.write("%s\n" % round(timer / 10, 3))
+            elif flag == "concat_seqs":
+                timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.gb --%s %s", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (flag, args), number=10)
+                sys.stdout.write("%s\n" % round(timer / 10, 3))
+            else:
+                timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.fa --%s %s", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (flag, args), number=10)
+                sys.stdout.write("%s\n" % round(timer / 10, 3))
+    elif in_args.command != "all" and in_args.command in opts_sb:
+        sys.stdout.write("%s: " % in_args.command)
         sys.stdout.flush()
-        timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.fa --%s %s", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (flag, args), number=10)
-        sys.stdout.write("%s\n" % round(timer / 10, 3))
+        if in_args.command == "bl2seq":
+            timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy seqs_25.fa --%s %s", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (in_args.command, opts_sb[in_args.command]), number=10)
+            sys.stdout.write("%s\n" % round(timer / 10, 3))
+        elif in_args.command == "concat_seqs":
+            timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.gb --%s %s", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (in_args.command, opts_sb[in_args.command]), number=10)
+            sys.stdout.write("%s\n" % round(timer / 10, 3))
+        else:
+            timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.fa --%s %s", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (in_args.command, opts_sb[in_args.command]), number=10)
+            sys.stdout.write("%s\n" % round(timer / 10, 3))
+    else:
+        sys.stdout.write("Invalid command\n")
