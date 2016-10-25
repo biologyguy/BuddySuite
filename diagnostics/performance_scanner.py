5f0c4a2c559c6a21d1e57e087d2185e4059b1c64
Sofia <alicarea@gmail.com>
2016-10-25 13:01:59 -0400

    Added iteration flag
    Default is 10

--- diagnostics/performance_scanner.py
+++ diagnostics/performance_scanner.py
@@ -13,28 +13,19 @@ if __name__ == '__main__':
     # parser.add_argument("file", help="input file", action="store")
     # parser.add_argument("-m", "--module", help="specify a single module", action='store')
     parser.add_argument("-c", "--command", help="specify a single module", action='store')
+    parser.add_argument("-i", "--iteration", help="specify iteration number", action='store', default=10)
     # use seq buddy for now, then ad the --module option
     in_args = parser.parse_args()
 
     # create a dictionary with the default parameters for each function (-c, --command)
     opts_sb = {
-        'list_ids': '3',
-        "num_seqs": "",
-        "lowercase": "",
-        "ave_seq_length": "",
-        "back_translate": "",
-        "clean_seq": "",
-        "complement": "",
-        "concat_seqs": "",
-        "count_codons": "",
-        "count_residues": "concatenate",
-        "degenerate_sequence": "1",
-        "delete_features": "TMD1",
-        "delete_large": "400",
-        "delete_metadata": "",
-        "delete_records": "Aae-",
-        "delete_repeats": "",
-        "delete_small": "100",
+        "ave_seq_length": "", "back_translate": "",
+        "clean_seq": "", "complement": "", "concat_seqs": "", "count_codons": "", "count_residues": "concatenate",
+        "degenerate_sequence": "1", "delete_features": "TMD1", "delete_large": "400", "delete_metadata": "",
+        "delete_records": "Aae-", "delete_repeats": "", "delete_small": "100", "extract_feature_sequences": "TMD1",
+        "extract_regions": ":50", "find_CpG": "-o genbank", "find_orfs": "-o genbank", "find_pattern": "\'LLL\' \'LLY\'",
+        "find_repeats": "3", "find_restriction_sites": "",
+        "list_ids": "3", "num_seqs": "", "lowercase": "",
     }
 
     # out of the dictionary: "bl2seq": "", "blast": "mnemiopsis_leidyi",
@@ -44,43 +35,43 @@ if __name__ == '__main__':
             sys.stdout.write("%s: " % flag)
             sys.stdout.flush()
             if flag == "bl2seq":
-                timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.fa --%s %s", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (flag, args), number=1)
-                sys.stdout.write("%s\n" % round(timer / 1, 3))
+                timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.fa --%s %s", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (flag, args), number=in_args.iteration)
+                sys.stdout.write("%s\n" % round(timer / in_args.iteration, 3))
             elif flag == "concat_seqs" or flag == "delete_metadata":
-                timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.gb --%s %s", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (flag, args), number=10)
-                sys.stdout.write("%s\n" % round(timer / 10, 3))
-            elif flag == "degenerate_sequence":
-                timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_nuc.fa --%s %s", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (flag, args), number=10)
-                sys.stdout.write("%s\n" % round(timer / 10, 3))
-            elif flag == "delete_features":
-                timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.gb --%s \'%s\'", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (flag, args), number=10)
-                sys.stdout.write("%s\n" % round(timer / 10, 3))
+                timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.gb --%s %s", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (flag, args), number=in_args.iteration)
+                sys.stdout.write("%s\n" % round(timer / in_args.iteration, 3))
+            elif flag == "degenerate_sequence" or flag == "find_CpG" or flag == "find_orfs" or flag == "find_restriction_sites":
+                timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_nuc.fa --%s %s", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (flag, args), number=in_args.iteration)
+                sys.stdout.write("%s\n" % round(timer / in_args.iteration, 3))
+            elif flag == "delete_features" or flag == "extract_feature_sequences" or flag == "extract_regions":
+                timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.gb --%s \'%s\'", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (flag, args), number=in_args.iteration)
+                sys.stdout.write("%s\n" % round(timer / in_args.iteration, 3))
             elif flag == "delete_records":
-                timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.fa --%s \'%s\'", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (flag, args), number=10)
-                sys.stdout.write("%s\n" % round(timer / 10, 3))
+                timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.fa --%s \'%s\'", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (flag, args), number=in_args.iteration)
+                sys.stdout.write("%s\n" % round(timer / in_args.iteration, 3))
             else:
-                timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.fa --%s %s", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (flag, args), number=10)
-                sys.stdout.write("%s\n" % round(timer / 10, 3))
+                timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.fa --%s %s", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (flag, args), number=in_args.iteration)
+                sys.stdout.write("%s\n" % round(timer / in_args.iteration, 3))
     elif in_args.command != "all" and in_args.command in opts_sb:
         sys.stdout.write("%s: " % in_args.command)
         sys.stdout.flush()
         if in_args.command == "bl2seq":
-            timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy seqs_25.fa --%s %s", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (in_args.command, opts_sb[in_args.command]), number=10)
-            sys.stdout.write("%s\n" % round(timer / 10, 3))
+            timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy seqs_25.fa --%s %s", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (in_args.command, opts_sb[in_args.command]), number=in_args.iteration)
+            sys.stdout.write("%s\n" % round(timer / in_args.iteration, 3))
         elif in_args.command == "concat_seqs" or in_args.command == "delete_metadata":
-            timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.gb --%s %s", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (in_args.command, opts_sb[in_args.command]), number=10)
-            sys.stdout.write("%s\n" % round(timer / 10, 3))
-        elif in_args.command == "degenerate_sequence":
-            timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_nuc.fa --%s %s", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (in_args.command, opts_sb[in_args.command]), number=10)
-            sys.stdout.write("%s\n" % round(timer / 10, 3))
-        elif in_args.command == "delete_features":
-            timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.gb --%s \'%s\'", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (in_args.command, opts_sb[in_args.command]), number=10)
-            sys.stdout.write("%s\n" % round(timer / 10, 3))
+            timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.gb --%s %s", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (in_args.command, opts_sb[in_args.command]), number=in_args.iteration)
+            sys.stdout.write("%s\n" % round(timer / in_args.iteration, 3))
+        elif in_args.command == "degenerate_sequence" or in_args.command == "find_CpG" or in_args.command == "find_orfs" or in_args.command == "find_restriction_sites":
+            timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_nuc.fa --%s %s", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (in_args.command, opts_sb[in_args.command]), number=in_args.iteration)
+            sys.stdout.write("%s\n" % round(timer / in_args.iteration, 3))
+        elif in_args.command == "delete_features" or in_args.command == "extract_feature_sequences" or in_args.command == "extract_regions":
+            timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.gb --%s \'%s\'", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (in_args.command, opts_sb[in_args.command]), number=in_args.iteration)
+            sys.stdout.write("%s\n" % round(timer / in_args.iteration, 3))
         elif in_args.command == "delete_records":
-            timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.fa --%s \'%s\'", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (in_args.command, opts_sb[in_args.command]), number=10)
-            sys.stdout.write("%s\n" % round(timer / 10, 3))
+            timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.fa --%s \'%s\'", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (in_args.command, opts_sb[in_args.command]), number=in_args.iteration)
+            sys.stdout.write("%s\n" % round(timer / in_args.iteration, 3))
         else:
-            timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.fa --%s %s", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (in_args.command, opts_sb[in_args.command]), number=10)
-            sys.stdout.write("%s\n" % round(timer / 10, 3))
+            timer = timeit.timeit('from subprocess import Popen, PIPE; Popen("seqbuddy All_pannexins_pep.fa --%s %s", shell=True, stderr=PIPE, stdout=PIPE).communicate()' % (in_args.command, opts_sb[in_args.command]), number=in_args.iteration)
+            sys.stdout.write("%s\n" % round(timer / in_args.iteration, 3))
     else:
         sys.stdout.write("Invalid command\n")
