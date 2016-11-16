41b1902ab68e85670cb9bf36ec180898812e89b1
Sofia <alicarea@gmail.com>
2016-11-16 09:09:06 -0500

    Removed local paths from script

--- diagnostics/create_boxplots.R
+++ diagnostics/create_boxplots.R
@@ -1,5 +1,4 @@
 
-setwd("/Users/depereirabarrsn/Buddysuite/diagnostics/results")
 
 
 ### Read in seqbuddy avg times
@@ -44,7 +43,7 @@ cl <- c(azul, black, black, black, verde, black, black, black, vermelho, black, 
 
 tempos <- data.frame(s1, z, z, z, a1, z, z, z, p1, z, z, z, z, z, z, s2, z, z, z, a2, z, z, z, p2, z, z, z, z, z, z, s3, z, z, z, a3, z, z, z, p3, z, z, z, z, z, z, s4, z, z, z, a4, z, z, z, p4, z)
 
-svg("/Users/depereirabarrsn/BuddySuite/diagnostics/results/figure.svg")
+svg("figure.svg")
 ## bottom, left, top, right
 par(mar=c(5,6,2,3)+0.1)
 #boxplot(sb[,1], ab[,1], pb[,1], sb[,2], ab[,2], pb[,2], sb[,3], ab[,3], pb[,3], sb[,4], ab[,4], pb[,4], at = c(1,2,3,6,7,8,11,12,13,16,17,18), col= cl, axes = FALSE, log = "y", border = cl)
