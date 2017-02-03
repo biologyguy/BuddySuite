b6df1f591fde1a1c4f16375cba3c7395059257ab
biologyguy <biologyguy@gmail.com>
2017-02-03 13:31:20 -0500

    Increment to V1.2.6

--- buddysuite/DatabaseBuddy.py
+++ buddysuite/DatabaseBuddy.py
@@ -80,7 +80,7 @@ FORMATS = ["ids", "accessions", "summary", "full-summary", "clustal", "embl", "f
            "fastq-solexa", "fastq-illumina", "genbank", "gb", "imgt", "nexus", "phd", "phylip", "seqxml",
            "stockholm", "tab", "qual"]
 CONFIG = br.config_values()
-VERSION = br.Version("DatabaseBuddy", 1, "2.5", br.contributors, {"year": 2017, "month": 2, "day": 3})
+VERSION = br.Version("DatabaseBuddy", 1, "2.6", br.contributors, {"year": 2017, "month": 2, "day": 3})
 
 GREY = "\033[90m"
 RED = "\033[91m"
